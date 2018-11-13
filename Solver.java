37af26070cecfb9435c9a74b58fd87c0cad4d60c
nal3x <nalex@aegean.gr>
2018-11-14 00:10:45 +0200

    working Solver implementation using predecessor path to parent node to track solution

--- Solver.java
+++ Solver.java
@@ -1,51 +1,50 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
     private int moves = -1;
-    private MinPQ<SearchNode> priorityQueue;
-    Queue<Board> solutionBoards;
+    private SearchNode examinedNode; // should contain goal board at the end
+    private boolean isSolvable;
 
     public Solver(Board initial) { // find a solution to the initial board (using the A* algorithm)
         // isSolvable is invoked from main client, so we know it's solvable
-        priorityQueue = new MinPQ<>();
-        solutionBoards = new Queue<>();
-        SearchNode examinedNode = new SearchNode(null, initial, ++moves);
+        MinPQ<SearchNode> priorityQueue = new MinPQ<>();
+        examinedNode = new SearchNode(null, initial, ++moves);
         priorityQueue.insert(examinedNode);
         while (true) {
             examinedNode = priorityQueue.delMin();
-            solutionBoards.enqueue(examinedNode.board);
-            // System.out.println("Dequed node:\n" + examinedNode); // DEBUG
-            if (examinedNode.board.isGoal()) {
+            System.out.println("Dequed node:\n" + examinedNode); // DEBUG
+            if (examinedNode.board.isGoal())
                 break;
-            } else {
-                moves++;
-                for (Board neighborBoard : examinedNode.board.neighbors()) {
-                    if (examinedNode.predecessor == null) {
+            moves++;
+            for (Board neighborBoard : examinedNode.board.neighbors()) {
+                if (examinedNode.predecessor != null) { // node has predecessor
+                    if (!neighborBoard.equals(examinedNode.predecessor.board)) // optimisation
                         priorityQueue.insert(new SearchNode(examinedNode, neighborBoard, moves));
-                    } else {
-                        if (!neighborBoard.equals(examinedNode.predecessor.board)) {
-                            priorityQueue.insert(new SearchNode(examinedNode, neighborBoard, moves));
-                        }
-                    }
-                }
-                // System.out.printf("******Priority Queue contents:******\n");
-                // for (SearchNode node : priorityQueue) {
-                //     System.out.println(node);
-                // }
+                } else
+                    priorityQueue.insert(new SearchNode(examinedNode, neighborBoard, moves));
             }
+            // System.out.printf("******Priority Queue contents:******\n");
+            // for (SearchNode node : priorityQueue) {
+            //     System.out.println(node);
+            // }
         }
     }
-
     public boolean isSolvable() { // is the initial board solvable?
-        return true;
+        return isSolvable;
     }
     public int moves() { // min number of moves to solve initial board; -1 if unsolvable
         return moves;
     }
     public Iterable<Board> solution() { // sequence of boards in a shortest solution; null if unsolvable
+        Stack<Board> solutionBoards = new Stack<>();
+        SearchNode node = examinedNode;
+        while (node != null) {
+            solutionBoards.push(node.board);
+            node = node.predecessor;
+        }
         return solutionBoards;
     }
 
@@ -58,10 +57,8 @@ public class Solver {
             for (int j = 0; j < n; j++)
                 blocks[i][j] = in.readInt();
         Board initial = new Board(blocks);
-
         // solve the puzzle
         Solver solver = new Solver(initial);
-
         // print solution to standard output
         if (!solver.isSolvable())
             StdOut.println("No solution possible");
@@ -76,19 +73,19 @@ public class Solver {
         private SearchNode predecessor;
         private Board board;
         private int priority;
-        private int hammingPriority;
         private int moves;
 
         SearchNode(SearchNode predecessor, Board board, int moves) {
             this.predecessor = predecessor;
             this.board = board;
             this.moves = moves;
-            this.hammingPriority = board.hamming() + this.moves;
             this.priority = board.manhattan() + this.moves; // caching of priority
         }
 
         @Override
         public int compareTo(SearchNode otherNode) {
+            // When 2 search nodes have same Manhattan priority, we break ties by comparing
+            // Manhattan distances and then Hamming distances of the two boards.
             if (this.priority < otherNode.priority) return -1;
             if (this.priority > otherNode.priority) return 1;
             if (this.board.manhattan() < otherNode.board.manhattan()) return -1;
